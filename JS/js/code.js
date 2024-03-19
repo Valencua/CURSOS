6b6e3c604f585439abd3647bf7c17943337b96a5
Valencua <110689742+Valencua@users.noreply.github.com>
2024-03-19 18:54:58 -0300

    Js clase 19 de marzo

--- JS/js/code.js
+++ JS/js/code.js
@@ -1,6 +1,70 @@
 
-numero1 = 10;
-numero2 = "8";
+dineroCofla = prompt("Cuanto dinero tienes cofla?");
+dineroRoberto = prompt("Cuanto dinero tienes Roberto?");
+dineroPedro = prompt("Cuanto dinero tienes Pedro?");
 
-frase = numero2.concat(numero1);
-document.write(frase);
+if (dineroCofla >= 0.6 && dineroCofla < 1){
+    alert("comprate el helado de agua");
+}
+else if (dineroCofla >= 1 && dineroCofla < 1.6){
+    alert("comprate el helado de crema");
+}
+else if (dineroCofla >= 1.6 && dineroCofla < 1.7){
+    alert("comprate el helado de heladix");
+}
+else if (dineroCofla >= 1.7 && dineroCofla < 1.8){
+    alert("comprate el helado de heladovich");
+}
+else if (dineroCofla >= 1.8 && dineroCofla < 2.9){
+    alert("comprate el helado de helardo");
+}
+else if (dineroCofla >= 2.9){
+    alert("comprate el helado con confites");
+}
+else {
+    alert("lo siento, pobre de mierda, no te alcanza para nada");
+}
+
+if (dineroPedro >= 0.6 && dineroPedro < 1){
+    alert("comprate el helado de agua");
+}
+else if (dineroPedro >= 1 && dineroPedro < 1.6){
+    alert("comprate el helado de crema");
+}
+else if (dineroPedro >= 1.6 && dineroPedro < 1.7){
+    alert("comprate el helado de heladix");
+}
+else if (dineroPedro >= 1.7 && dineroPedro < 1.8){
+    alert("comprate el helado de heladovich");
+}
+else if (dineroPedro >= 1.8 && dineroPedro < 2.9){
+    alert("comprate el helado de helardo");
+}
+else if (dineroPedro >= 2.9){
+    alert("comprate el helado con confites");
+}
+else {
+    alert("lo siento, pobre de mierda, no te alcanza para nada");
+}
+
+if (dineroRoberto >= 0.6 && dineroRoberto < 1){
+    alert("comprate el helado de agua");
+}
+else if (dineroRoberto >= 1 && dineroRoberto < 1.6){
+    alert("comprate el helado de crema");
+}
+else if (dineroRoberto >= 1.6 && dineroRoberto < 1.7){
+    alert("comprate el helado de heladix");
+}
+else if (dineroRoberto >= 1.7 && dineroRoberto < 1.8){
+    alert("comprate el helado de heladovich");
+}
+else if (dineroRoberto >= 1.8 && dineroRoberto < 2.9){
+    alert("comprate el helado de helardo");
+}
+else if (dineroRoberto >= 2.9){
+    alert("comprate el helado con confites");
+}
+else {
+    alert("lo siento, pobre de mierda, no te alcanza para nada");
+}
